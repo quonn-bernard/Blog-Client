83062417290c6219f21511032f0d0850bd0a632e
quonn <qber83@gmail.com>
2019-05-25 07:16:00 -0700

    Updated Landing Page styling and added icons

--- blog/src/Components/Landing/Landing.js
+++ blog/src/Components/Landing/Landing.js
@@ -1,25 +1,35 @@
 import React from "react";
 import "./Landing.css";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark ,faSearch } from '@fortawesome/free-solid-svg-icons'
 
 class Landing extends React.Component {
+  
   render() {
+    const style = {
+      color: "red",
+      fontSize: "40px"
+    }
+
+    const bookmark = <FontAwesomeIcon icon={faBookmark} style={style}/>
+    const searchGlass = <FontAwesomeIcon icon={faSearch} className="search"/>
+
     return (
       <div>
         <section id="banner" className="section section-grid">
           <div className="section-grid-item" />
           <div className="section-grid-item">
-            <h1>Video Finder App</h1>
-            <p>
-
-              The Video Finder App allows users to search videos at youtube.com. If the user finds a video that they found particularly, entertaining,
-              useful, or just well done in general, they can bookmark it, rate it from 1-5 stars and leave somefeed back about the video and how it helped them. The bookmarks
-              are then added to a  scrollable "feed", that will act as a landing page for authenticated users. Login and Registration are not required to use the system.
-              The DEMO credentials are username: "demo1234" password: "Demo1234!"; Signing in will give users the ability to add bookmarks to the feed.
-       </p>
+            <h1>V-MARKER</h1>
+            {bookmark}
+            <p className="landing-content-text">
+              V-MARKER allows users to search videos via youtube api. If the user finds a video that they found useful
+             or just well done in general, they can bookmark it, rate it and leave somefeed back about how the video was of use. 
+              The DEMO credentials are username: "demo1234" password: "Demo1234!"; Signing in will give users the ability to add bookmarks to the "feed".
+            </p>
             <div className="landing-btns-box">
               <Link className="btn" to="/blogFeed">
-                Find Tutorials
+                {searchGlass} Find Tutorials
             </Link>
               <Link className="btn" to="/login">
                 Login as a Guest
