378e5df7cf7b894ce4df0c2d733fc470ff232ccc
quonn <qber83@gmail.com>
2019-05-25 08:00:31 -0700

    Removed uneeded comments

--- blog/src/Components/Footer/Footer.js
+++ blog/src/Components/Footer/Footer.js
@@ -2,13 +2,11 @@ import React from 'react';
 import "./Footer.css"
 
 export default function Footer() {
-    // let endpoints = ["register","login",""]
-    // if(window.location.href !== )
-    // console.log(window.location.href)
     return (
         <div className="Footer fixedBottom">
-         {/* <div className={(window.location.href !== `http://localhost:3000/blogFeed`) ? "Footer fixedBottom" : "Footer"}> */}
+        
             <small>© Copyright 2019. Designed by Quonn Bernard</small>
+
         </div>
     )
 }
