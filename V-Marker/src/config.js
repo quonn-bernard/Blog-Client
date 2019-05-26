3aa5f9f70a006306156a616f05e1b9f2ea06cea1
quonn <qber83@gmail.com>
2019-05-26 16:16:51 -0700

    removed comment

--- V-Marker/src/config.js
+++ V-Marker/src/config.js
@@ -1,5 +1,4 @@
 export default {
-    // API_ENDPOINT : 'https://rocky-fortress-82479.herokuapp.com/api',
     API_ENDPOINT : 'https://protected-tundra-30619.herokuapp.com/api',
     TOKEN_KEY: 'v-marker-client-auth-token',
     API_KEY: process.env.REACT_APP_API_KEY
