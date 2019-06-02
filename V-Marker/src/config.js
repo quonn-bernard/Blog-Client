31228f7c0230f21975077fb7a45707afe2dbd2b9
quonn <qber83@gmail.com>
2019-06-01 17:14:44 -0700

    deleted comment

--- V-Marker/src/config.js
+++ V-Marker/src/config.js
@@ -1,5 +1,5 @@
 export default {
     API_ENDPOINT : 'https://protected-tundra-30619.herokuapp.com/api',
+    //API_ENDPOINT : 'http://localhost:8000/api',
     TOKEN_KEY: 'v-marker-client-auth-token',
-    API_KEY: process.env.REACT_APP_API_KEY
   }
